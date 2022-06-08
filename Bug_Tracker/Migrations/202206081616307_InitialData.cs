3c813289148addf06f7b38cd6043082cf9213420
DavidSMoyer <poinky_m@hotmail.com>
2022-06-08 11:18:21 -0500

    Added data migration, modified connection string

---
+++ Bug_Tracker/Migrations/202206081616307_InitialData.cs
@@ -0,0 +1,272 @@
+﻿namespace Bug_Tracker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialData : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ManagerId = c.String(maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.ManagerId)
+                .Index(t => t.ManagerId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.ProjectUsers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProjectId = c.Int(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Projects", t => t.ProjectId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.ProjectId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.TicketPriorities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tickets",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        OwnerId = c.String(maxLength: 128),
+                        Content = c.String(),
+                        AssignedDevId = c.String(maxLength: 128),
+                        ProjectId = c.Int(nullable: false),
+                        StatusId = c.Int(nullable: false),
+                        TypeId = c.Int(nullable: false),
+                        PriorityId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.AssignedDevId)
+                .ForeignKey("dbo.AspNetUsers", t => t.OwnerId)
+                .ForeignKey("dbo.TicketPriorities", t => t.PriorityId, cascadeDelete: true)
+                .ForeignKey("dbo.Projects", t => t.ProjectId, cascadeDelete: true)
+                .ForeignKey("dbo.TicketStatus", t => t.StatusId, cascadeDelete: true)
+                .ForeignKey("dbo.TicketTypes", t => t.TypeId, cascadeDelete: true)
+                .Index(t => t.OwnerId)
+                .Index(t => t.AssignedDevId)
+                .Index(t => t.ProjectId)
+                .Index(t => t.StatusId)
+                .Index(t => t.TypeId)
+                .Index(t => t.PriorityId);
+            
+            CreateTable(
+                "dbo.TicketAttatchments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TicketId = c.Int(nullable: false),
+                        FilePath = c.String(),
+                        Description = c.String(),
+                        Created = c.DateTime(nullable: false),
+                        FileUrl = c.String(),
+                        OwnerId = c.String(),
+                        User_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Tickets", t => t.TicketId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.User_Id)
+                .Index(t => t.TicketId)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.TicketComments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TicketId = c.Int(nullable: false),
+                        Comment = c.String(),
+                        OwnerId = c.String(maxLength: 128),
+                        Created = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.OwnerId)
+                .ForeignKey("dbo.Tickets", t => t.TicketId, cascadeDelete: true)
+                .Index(t => t.TicketId)
+                .Index(t => t.OwnerId);
+            
+            CreateTable(
+                "dbo.TicketHistories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TicketId = c.Int(nullable: false),
+                        Property = c.String(),
+                        OldValue = c.String(),
+                        NewValue = c.String(),
+                        Changed = c.DateTime(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Tickets", t => t.TicketId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.TicketId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.TicketStatus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TicketTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tickets", "TypeId", "dbo.TicketTypes");
+            DropForeignKey("dbo.Tickets", "StatusId", "dbo.TicketStatus");
+            DropForeignKey("dbo.Tickets", "ProjectId", "dbo.Projects");
+            DropForeignKey("dbo.Tickets", "PriorityId", "dbo.TicketPriorities");
+            DropForeignKey("dbo.Tickets", "OwnerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.TicketHistories", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.TicketHistories", "TicketId", "dbo.Tickets");
+            DropForeignKey("dbo.TicketComments", "TicketId", "dbo.Tickets");
+            DropForeignKey("dbo.TicketComments", "OwnerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.TicketAttatchments", "User_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.TicketAttatchments", "TicketId", "dbo.Tickets");
+            DropForeignKey("dbo.Tickets", "AssignedDevId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.ProjectUsers", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.ProjectUsers", "ProjectId", "dbo.Projects");
+            DropForeignKey("dbo.Projects", "ManagerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.TicketHistories", new[] { "UserId" });
+            DropIndex("dbo.TicketHistories", new[] { "TicketId" });
+            DropIndex("dbo.TicketComments", new[] { "OwnerId" });
+            DropIndex("dbo.TicketComments", new[] { "TicketId" });
+            DropIndex("dbo.TicketAttatchments", new[] { "User_Id" });
+            DropIndex("dbo.TicketAttatchments", new[] { "TicketId" });
+            DropIndex("dbo.Tickets", new[] { "PriorityId" });
+            DropIndex("dbo.Tickets", new[] { "TypeId" });
+            DropIndex("dbo.Tickets", new[] { "StatusId" });
+            DropIndex("dbo.Tickets", new[] { "ProjectId" });
+            DropIndex("dbo.Tickets", new[] { "AssignedDevId" });
+            DropIndex("dbo.Tickets", new[] { "OwnerId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.ProjectUsers", new[] { "UserId" });
+            DropIndex("dbo.ProjectUsers", new[] { "ProjectId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Projects", new[] { "ManagerId" });
+            DropTable("dbo.TicketTypes");
+            DropTable("dbo.TicketStatus");
+            DropTable("dbo.TicketHistories");
+            DropTable("dbo.TicketComments");
+            DropTable("dbo.TicketAttatchments");
+            DropTable("dbo.Tickets");
+            DropTable("dbo.TicketPriorities");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.ProjectUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Projects");
+        }
+    }
+}
