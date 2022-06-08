1ee73327ac454627edc9403bfe23c04ada876007
DavidSMoyer <poinky_m@hotmail.com>
2022-06-08 10:39:43 -0500

    Removed enums, changed ticketattatchment to db specs

--- Bug_Tracker/Models/TicketAttatchment.cs
+++ Bug_Tracker/Models/TicketAttatchment.cs
@@ -10,6 +10,11 @@ namespace Bug_Tracker.Models
         public int Id { get; set; }
         public int TicketId { get; set; }
         public virtual Ticket Ticket { get; set; }
-        public string Location { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string FileUrl { get; set; }
+        public string OwnerId { get; set; }
+        public virtual ApplicationUser User { get; set; }
     }
 }
